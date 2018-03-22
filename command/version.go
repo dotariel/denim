6a666004e8003648663449e5daa4092982227b55
Ariel Alonso <xsaint32@gmail.com>
2018-03-22 09:19:22 -0400

    Version information is more concise now

--- command/version.go
+++ command/version.go
@@ -13,8 +13,7 @@ func Version() *cobra.Command {
 		Use:   "version",
 		Short: "display version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf(" %-12s %s\n", "Version:", app.Version)
-			fmt.Printf(" %-12s %s\n", "Date:", app.BuildDate)
+			fmt.Printf("denim v%s (%s)", app.Version, app.BuildDate)
 			return
 		},
 	}
